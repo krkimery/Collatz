edd406607a0aaf171bb8c2d4bc9532b6a92857fd
Kyle Kimery <kylekimery@utexas.edu>
2017-01-30 17:16:04 -0600

    Fixed redundant code in Collatz.py, added tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -33,25 +33,45 @@ class TestCollatz (TestCase):
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
 
+    def test_read_2(self):
+        s = "5 100\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  5)
+        self.assertEqual(j, 100)
+
+    def test_read_3(self):
+        s = "1000 10\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i,  1000)
+        self.assertEqual(j, 10)
+
     # ----
     # eval
     # ----
 
     def test_eval_1(self):
-        v = collatz_eval(1, 1)  #test whether boundaries of range are set correctly
-        self.assertEqual(v, 1)
+        v = collatz_eval(1, 10)
+        self.assertEqual(v, 20)
 
     def test_eval_2(self):
-        v = collatz_eval(1, 7)  #tests whether boundaries are inclusive
-        self.assertEqual(v, 17)
+        v = collatz_eval(100, 200)
+        self.assertEqual(v, 125)
 
     def test_eval_3(self):
-        v = collatz_eval(3, 2)
-        self.assertEqual(v, 8)
+        v = collatz_eval(201, 210)
+        self.assertEqual(v, 89)
 
     def test_eval_4(self):
-        v = collatz_eval(1,999999)
-        self.assertEqual(v, 525)
+        v = collatz_eval(900, 1000)
+        self.assertEqual(v, 174)
+
+    def test_eval_5(self):
+        v = collatz_eval(1, 1000)
+        self.assertEqual(v, 179)
+
+    def test_eval_6(self):
+        v = collatz_eval(10, 1)
+        self.assertEqual(v, 20)
 
     # -----
     # print
@@ -62,16 +82,46 @@ class TestCollatz (TestCase):
         collatz_print(w, 1, 10, 20)
         self.assertEqual(w.getvalue(), "1 10 20\n")
 
+    def test_print_2(self):
+        w = StringIO()
+        collatz_print(w, 100, 200, 125)
+        self.assertEqual(w.getvalue(), "100 200 125\n")
+
+    def test_print_3(self):
+        w = StringIO()
+        collatz_print(w, 10, 1, 20)
+        self.assertEqual(w.getvalue(), "10 1 20\n")
+
+    def test_print_4(self):
+        w = StringIO()
+        collatz_print(w, 800, 899, 179)
+        self.assertEqual(w.getvalue(), "800 899 179\n")
+
+
     # -----
     # solve
     # -----
 
     def test_solve(self):
-        r = StringIO("1 2\n1 10\n11 15\n1 15\n")
+        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
         self.assertEqual(
-            w.getvalue(), "1 2 2\n1 10 20\n11 15 18\n1 15 20\n")
+            w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
+
+    def test_solve_2(self):
+        r = StringIO("900 1000\n800 899\n6000 6200\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "900 1000 174\n800 899 179\n6000 6200 262\n")
+
+    def test_solve_3(self):
+        r = StringIO("1 10\n10 1\n10 10\n")
+        w = StringIO()
+        collatz_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "1 10 20\n10 1 20\n10 10 7\n")
 
 # ----
 # main
@@ -81,26 +131,25 @@ if __name__ == "__main__":
     main()
 
 """ #pragma: no cover
-% coverage3 run --branch TestCollatz.py >  TestCollatz.out 2>&1
+% coverage-3.5 run --branch TestCollatz.py >  TestCollatz.out 2>&1
 
 
+% cat TestCollatz.out
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.000s
+OK
 
-% coverage3 report -m                   >> TestCollatz.out
+
+% coverage-3.5 report -m                   >> TestCollatz.out
 
 
 
 % cat TestCollatz.out
 .......
 ----------------------------------------------------------------------
-Ran 7 tests in 0.001s
-
+Ran 7 tests in 0.000s
 OK
-Name          Stmts   Miss Branch BrMiss  Cover   Missing
----------------------------------------------------------
-Collatz          18      0      6      0   100%
-TestCollatz      33      1      2      1    94%   79
----------------------------------------------------------
-TOTAL            51      1      8      1    97%
 Name             Stmts   Miss Branch BrPart  Cover   Missing
 ------------------------------------------------------------
 Collatz.py          12      0      2      0   100%
